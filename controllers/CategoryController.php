026fae139e9385743b297ff6ae6517f71bad9de8
Grigoriy Kolodyazhniy <shakolo@gmail.com>
2017-12-14 13:01:10 +0200

    Yii::EshopLesson7 - add /category/29/ view

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -19,4 +19,11 @@ class CategoryController extends AppController
 //        debug($hits);
     return $this->render('index', compact('hits'));
     }
+
+    public function actionView($id) {
+        $id = Yii::$app->request->get('id');
+//        debug($id);
+        $products = Product::find()->where(['category_id' => $id])->all();
+        return $this->render('view', compact('products'));
+    }
 }
--- views/site/index.php
+++ views/site/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /* @var $this yii\web\View */
-
+use yii\helpers\Html;
 $this->title = 'My Yii Application';
 ?>
 
